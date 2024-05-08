4377680f4d936627b6ad0af1654a4641aa183645
yang <zhiyang520679@gmail.com>
2024-05-08 11:51:55 +0200

    update

---
+++ app/src/main/java/com/zy/proyecto_final/fragment/OrdersDetailsFragment.kt
@@ -0,0 +1,68 @@
+package com.zy.proyecto_final.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.zy.proyecto_final.R
+import com.zy.proyecto_final.databinding.FragmentOrderDetailsBinding
+import com.zy.proyecto_final.fragments.ProductsFragment
+import com.zy.proyecto_final.recyclerviewadapter.OrdersRecyclerViewAdapter
+import com.zy.proyecto_final.viewmodel.OrderViewModel
+import com.zy.proyecto_final.viewmodel.UserViewModel
+
+class OrdersDetailsFragment : Fragment() {
+    private val viewmodel: OrderViewModel by activityViewModels()
+    private val userViewModel: UserViewModel by activityViewModels()
+
+    private  lateinit var binding: FragmentOrderDetailsBinding
+    private var view:View?=null
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_order_details, container, false)
+        binding.lifecycleOwner = this
+        // Obteniendo el ID de la orden
+        val orderId = arguments!!.getString("orderId")
+
+        binding.titleOrder.setNavigationOnClickListener {
+            //replace fragment
+            activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.fragmentContainerView, ProductsFragment())?.commit()
+
+        }
+        var order = viewmodel.getOrder(orderId!!.toLong())
+
+        binding.phone  = order.phone
+        binding.address = order.address
+        binding.payment =order.payment
+        binding.total = order.total
+        binding.titleOrder.title = order.orderStatus
+        var time =order.time
+        var date = order.date
+        binding.date = date
+        binding.time = time
+        val recyclerView = view?.findViewById<RecyclerView>(R.id.listado)
+        recyclerView?.layoutManager = LinearLayoutManager(context)
+        this.viewmodel.items.value?.let {
+            recyclerView?.adapter = OrdersRecyclerViewAdapter(it.toMutableList())
+        }
+        
+        
+        
+        return binding.root
+
+
+    }
+
+    companion object {
+        fun newInstance() = OrdersDetailsFragment()
+    }
+}
--- app/src/main/java/com/zy/proyecto_final/fragments/OrdersFragment.kt
+++ app/src/main/java/com/zy/proyecto_final/fragments/OrdersFragment.kt
@@ -5,18 +5,51 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.zy.proyecto_final.R
+import com.zy.proyecto_final.fragment.OrdersDetailsFragment
+import com.zy.proyecto_final.pojo.Order
+import com.zy.proyecto_final.pojo.Product
+import com.zy.proyecto_final.recyclerviewadapter.OrdersRecyclerViewAdapter
+import com.zy.proyecto_final.viewmodel.OrderViewModel
+
 
 class OrdersFragment : Fragment() {
+    private val viewmodel : OrderViewModel by activityViewModels()
     private var view: View? = null
+
+    var detail_click: ((Int, Product) -> Unit)? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val orderStatus = arguments?.getString("orderStatus")
         view = inflater.inflate(R.layout.fragment_orders, container, false)
+        val recyclerView = view?.findViewById<RecyclerView>(R.id.listado)
+        recyclerView?.layoutManager = LinearLayoutManager(context)
+        this.viewmodel.items.value?.let {
+            recyclerView?.adapter = OrdersRecyclerViewAdapter(it.toMutableList(), orderStatus!!)
 
-        return view
+        }
+        (view?.findViewById<RecyclerView>(R.id.listado)!!.adapter as OrdersRecyclerViewAdapter).detail_click =
+            { position: Int, item: Order ->
+                run {
+                    this.viewmodel.selectedorder = item
+                   this.detail_click?.let { it -> it(position, item) }
+                    var fm: FragmentManager = parentFragmentManager
+                    val args = Bundle()
+                    args.putLong("orderId", item.id); // Suponiendo que orderId es el ID de la orden.
+                    val fragment = OrdersDetailsFragment.newInstance()
+                    fragment.setArguments(args);
+                    // pasa el id de la orden
+                    fm.beginTransaction().replace(R.id.fragmentContainerView, fragment).commit();
+                }
+
+                return view
+            }
     }
     companion object {
         fun newInstance(orderStatus: String): OrdersFragment {
